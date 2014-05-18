762c03e7b6173bf81b46f81969d0e66f5708b3b5
Klaus Huber <klaus.huber@fernuni-hagen.de>
2014-05-18 19:54:30 +0200

    Die Projekte aus dem Buch Java-Head First

---
+++ src/Schlagzeug.java
@@ -0,0 +1,15 @@
+
+public class Schlagzeug {
+    
+    boolean tomTom = true;
+    boolean snare = true;
+    
+    void spieleSnare() {
+        System.out.println("pamm pamm pa-pamm");
+    }
+        
+    void spieleTomTom() {
+        System.out.println("dumm dumm du-dumm");
+    }
+
+}
---
+++ src/SchlagzeugTestlauf.java
@@ -0,0 +1,23 @@
+
+public class SchlagzeugTestlauf {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Schlagzeug s = new Schlagzeug();
+        
+        
+        if (s.snare == true) {
+            s.spieleSnare();
+        }
+        
+        s.spieleTomTom();
+        
+    }
+
+     
+    
+}
